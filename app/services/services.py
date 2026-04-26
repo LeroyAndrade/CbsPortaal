6c722cd660070250f2e3e94494e57d8df8d68023
LeroyAndrade <leroy_andrade@hotmail.com>
2026-04-26 14:51:23 +0200

    Mobile friendly API artikelen van CBS
    Pas in services.py de top variabel in de var url, aan voor meer artikelen

--- app/services/services.py
+++ app/services/services.py
@@ -1,16 +1,19 @@
+import httpx
 import requests
 
 class ArticleService:
-    
+
     @staticmethod
     def get_latest_cbs_article():
-        url = "https://www.cbs.nl/odata/v1/Articles?$top=5&$orderby=ReleaseTime%20desc&$select=Title,Url,Image,Themes,TaxonomyTags,LeadText"
+        # url = "https://www.cbs.nl/odata/v1/Articles?$filter=endswith(UniqueId, '-nl-nl')&$skip=3&$top=6&$orderby=ReleaseTime%20desc&$select=Title,Url,Image,Themes,TaxonomyTags,LeadText"
+        url = "https://www.cbs.nl/odata/v1/Articles?$filter=Language eq 'nl-NL'&$skip=2&$top=3&$orderby=ReleaseTime%20desc&$select=Title,Url,Image,Themes,TaxonomyTags,LeadText"
 
         try:
-            r = requests.get(url)
-            data = r.json()
-            body_text = data["value"]
-            return body_text
+            with httpx.Client() as client:
+                r = client.get(url)
+                r.raise_for_status()
+                data = r.json()
+                return data.get("value", [])
         except Exception as e:
             print(f"Artikelen fetchen error: \n{e}")
             return []
