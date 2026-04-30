f6121e080e98d8f2c0e938300c342a2f71fb818d
LeroyAndrade <leroy_andrade@hotmail.com>
2026-04-30 15:58:19 +0200

    Debug uitgezet

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ app = create_app()
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port='5002', debug=True)
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5002)), debug=False)
+
+    # app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5002)))
